b2b3604198866dd4c49d4b3cbfcb60f621d5e82b
Felipe R. Goncalves <felipefrg@hotmail.com>
2023-02-23 20:09:24 -0300

    Adding BookingService Interface

---
+++ src/ALTENBooking.Application/DTOs/ResultDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALTENBooking.Application.DTOs
+{
+    public class ResultDTO
+    {
+        public bool HasError { get; set; }
+        public string Message { get; set; }        
+    }
+
+    public class ResultDTO<T> : ResultDTO where T : class
+    {        
+        public T Data { get; set; }
+    }
+}
---
+++ src/ALTENBooking.Application/Interfaces/IBookingService.cs
@@ -0,0 +1,20 @@
+﻿using ALTENBooking.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALTENBooking.Application.Interfaces
+{
+    public interface IBookingService
+    {
+        bool IsRoomAvailable(DateTime startDate, DateTime endDate);
+
+        ResultDTO DoReservation(Guid CustomerId, Guid RoomId, DateTime startDate, DateTime endDate);
+
+        ResultDTO CancelReservation(Guid CustomerId, Guid RoomId, DateTime startDate, DateTime endDate);
+
+        ResultDTO ModifyReservation(Guid CustomerId, Guid RoomId, DateTime startDate, DateTime endDate);
+    }
+}
