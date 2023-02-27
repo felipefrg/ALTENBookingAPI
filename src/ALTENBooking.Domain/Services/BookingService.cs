9ac921e8f87c8c5092c8e54eef41c61914c07a52
Felipe R. Goncalves <felipefrg@hotmail.com>
2023-02-27 00:05:48 -0300

    Adding doc files

--- src/ALTENBooking.Domain/Services/BookingService.cs
+++ src/ALTENBooking.Domain/Services/BookingService.cs
@@ -2,11 +2,6 @@
 using ALTENBooking.Domain.Models;
 using ALTENBooking.Domain.Queries;
 using ALTENBooking.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ALTENBooking.Domain.Services
 {
